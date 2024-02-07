d5958ec6be495df33dbd761ed552a99422e242c5
Novarc <13143105+novarc@user.noreply.gitee.com>
2024-02-07 22:49:55 +0800

    start3

--- xmake.lua
+++ xmake.lua
@@ -2,7 +2,7 @@ add_rules("mode.debug", "mode.release")
 add_requires("glfw")
 target("nova")
     set_kind("binary")
-    add_files("src/*.cpp")
+    add_files("src/**.cpp")
     add_packages("glfw")
 
 --
