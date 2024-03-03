faf20382e0c25a69e219fd8769e88c0f15f5bce3
Novarc <13143105+novarc@user.noreply.gitee.com>
2024-03-03 17:15:33 +0800

    android fix

--- android/androidCore/jni/log/JniLog.h
+++ android/androidCore/jni/log/JniLog.h
@@ -7,8 +7,8 @@
 #ifndef NATIVEOPNEGLDEMO_JNILOG_H
 #define NATIVEOPNEGLDEMO_JNILOG_H
 
-#define LOGD(FORMAT, ...)  __android_log_print (ANDROID_LOG_DEBUG,"jniLog",FORMAT,##__VA_ARGS__)
-#define LOGE(FORMAT, ...) __android_log_print (ANDROID_LOG_ERROR,"jniLog",FORMAT,##__VA_ARGS__)
+#define LOGD(FORMAT, ...)  __android_log_print (ANDROID_LOG_DEBUG,"jniLognova",FORMAT,##__VA_ARGS__)
+#define LOGE(FORMAT, ...) __android_log_print (ANDROID_LOG_ERROR,"jniLognova",FORMAT,##__VA_ARGS__)
 
 
 #endif //NATIVEOPNEGLDEMO_JNILOG_H
